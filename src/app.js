98d5340052965e52a79c939fa5da808c6e8259ff
Gaurang Patel <93378047+Patel-Gaurang-12@users.noreply.github.com>
2023-07-29 11:21:54 +0530

    first

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+// dot env config and database config
+require("dotenv").config()
+require("./config/dbConfig").dbConnect()
+
+// enable statements
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// users routes 
+const clientRouters = require("./routes/clientRoutes")
+app.use("/distributer/api/v1/public/client", clientRouters);
+
+// company routes
+const companyRouters = require("./routes/comapnyRoutes");
+app.use("/distributer/api/v1/public/company", companyRouters)
+
+// item routers
+const itemsRouters = require("./routes/companyWiseItemRouters");
+app.use("/distributer/api/v1/public/item", itemsRouters)
+
+app.listen(process.env.PORT, () => console.log("server listern on port number : ", process.env.PORT))
---
+++ src/routes/companyWiseItemRouters.js
@@ -0,0 +1,10 @@
+const router = require("express").Router()
+const itemController = require("../controller/companyWiseItemController")
+
+router.post("/additem", itemController.addItem)
+router.get("/items", itemController.getItems)
+router.put("/item/:id", itemController.updateItem)
+router.get("/item/:id", itemController.getItemById)
+router.delete("/item/:id", itemController.deleteItem)
+
+module.exports = router
