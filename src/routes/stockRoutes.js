f21b359c04d1a5ee9ef2280b2cfcdad334a312d7
DipPanchal <panchaldip029@gmail.com>
2023-08-20 15:44:04 +0530

    ddasfs

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -2,6 +2,6 @@ const router = require("express").Router()
 const stockController = require("../controller/stockController")
 
 router.get("/stock", stockController.getStockData)
-router.get("/stock-history/:id", stockController.getStockHistory)
+router.put("/stock-history/:id", stockController.getStockHistory)
 
 module.exports = router
